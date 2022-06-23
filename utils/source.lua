ccef46f95f0048e6fa6b080891a5a0c478a0e04f
Stebulous <sky34416@gmail.com>
2022-06-22 20:53:52 -0400

    add utils:FindObj

--- utils/source.lua
+++ utils/source.lua
@@ -1,7 +1,7 @@
 local utils = {}
 
 -- // Version
-utils.Version = '1.2.0'
+utils.Version = '1.3.0-beta'
 
 -- // Services
 utils.Players = game:GetService("Players")
@@ -16,6 +16,7 @@ utils.Character = utils.Player.Character
 utils.Humanoid = utils.Character:FindFirstChild("Humanoid")
 utils.HRP = utils.Character:FindFirstChild("HumanoidRootPart")
 utils.Camera = workspace.CurrentCamera
+utils.CoreGui = game:GetService("CoreGui")
 utils.PlayerCount = tostring(#utils.Players:GetPlayers())
 utils.FOV = utils.Camera.FieldOfView
 utils.Health = utils.Humanoid.Health
@@ -47,7 +48,7 @@ utils.RunService.RenderStepped:Connect(function (delta)
 end)
 
 -- // Functions
-local function log(type, message)
+local function log( type, message )
     if type == "info" then
         print("[Vision/Utils] INFO: " .. tostring(message))
     elseif type == "warn" then
@@ -58,11 +59,11 @@ local function log(type, message)
 end
 
 
-function utils:LoadURL(url)
+function utils:LoadUrl( url )
     return loadstring(game:HttpGet(url))()
 end
 
-function utils:Try(func, name, verbose)
+function utils:Try( func, name, verbose )
     local success, err = pcall(function() func() end)
     if not success and verbose then
         log("error", string.format('Function "%s" failed to run, Error: %s', name, err))
@@ -91,6 +92,34 @@ function utils:GetExecutor()
     return exploit
 end
 
+function utils:FindObj( name, showall )
+    showall = showall or false
+
+    local starttime = tick()
+    if not showall then
+        local v = game:FindFirstChild(name, true)
+            if v and v.Name == name then
+                local timetaken = string.sub((tick()) - (starttime), 1, 4)
+                log('info', 'Found object: "' .. name .. '" in ' .. timetaken .. ' ms.')
+                return v
+            else
+                return log('warn', 'Object not found: "' .. name .. '"')
+            end
+    else
+        local found = {}
+        for _,v in pairs( workspace:GetDescendants(), utils.ReplicatedStorage:GetDescendants(), utils.CoreGui:GetDescendants() ) do
+            if v.Name == name then
+                local timetaken = string.sub((tick()) - (starttime), 1, 4)
+                log('info', 'Found object: "' .. name .. '" in ' .. timetaken .. ' ms.')
+                table.insert(found, v)
+            end
+        end
+        return found
+        end
+end
+
 log('info', 'Initialized.')
 
-return utils;
+print(unpack(utils:FindObj('DoMoNiCgt', true)))
+
+--return utils;
