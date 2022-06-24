b0dc0a877eac9bfa208867b3d3407db31d26c221
Stebulous <sky34416@gmail.com>
2022-06-23 21:53:44 -0400

    added utils.Icons

--- utils/source.lua
+++ utils/source.lua
@@ -32,6 +32,38 @@ utils.MaxHealth = utils.Humanoid.MaxHealth
 utils.Health = utils.Humanoid.Health
 utils.TeamColor = utils.Player.TeamColor
 
+utils.Icons = {
+    FluentIcons = {
+		Home = "rbxassetid://9792462652",
+		Aimbot = "rbxassetid://9766671041",
+		Visuals = "rbxassetid://9766673555",
+		Player = "rbxassetid://9766672602",
+		Misc = "rbxassetid://9766671943",
+		Settings = "rbxassetid://9766674082",
+		Credits = "rbxassetid://9766675093",
+		Exit = "rbxassetid://9766676906"
+	},
+	FeatherIcons = {
+		Home = "rbxassetid://9792650361",
+		Aimbot = "rbxassetid://9792632523",
+		Visuals = "rbxassetid://9792631281",
+		Player = "rbxassetid://9792631906",
+		Misc = "rbxassetid://9792634811",
+		Settings = "rbxassetid://9792633222",
+		Credits = "rbxassetid://9792634075",
+		Exit = "rbxassetid://9792635572"
+	},
+	NotificationIcons = {
+		Success = "rbxassetid://9838874163",
+		Warning = "rbxassetid://9838873385",
+		Error = "rbxassetid://9838876113",
+		Informational = "rbxassetid://9838877673",
+		Custom = "rbxassetid://9838878267"
+	},
+	VisionLogo = "rbxassetid://9795408363"
+}
+
+
 utils.Uptime = {
     Days = math.floor( elapsedTime() / 86400 ),
     Hours = math.floor( elapsedTime() / 3600 ),
